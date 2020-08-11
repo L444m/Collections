d4a4c8f7498e66f28e649d8bf15a5ce3996a9ded
Liam <stuartbaugh246@gmail.com>
2020-08-11 18:39:31 +0800

    Optimized utils code style.

--- commons/src/main/java/com/liamma/commons/utils/IOUtils.java
+++ commons/src/main/java/com/liamma/commons/utils/IOUtils.java
@@ -5,7 +5,6 @@ import androidx.annotation.Nullable;
 import java.io.Closeable;
 import java.io.IOException;
 
-
 /**
  * I/O Utils.
  * Created by Liam on 2018/4/17.
@@ -13,22 +12,24 @@ import java.io.IOException;
 public final class IOUtils {
 
     private IOUtils() {
-        throw new UnsupportedOperationException("cannot be instantiated");
+        throw new UnsupportedOperationException("cannot be instantiated.");
     }
 
     /**
      * Closes the IO streams.
      */
     public static void close(@Nullable final Closeable... closeables) {
-        if (EmptyUtils.isEmpty(closeables)) return;
-
+        if (EmptyUtils.isEmpty(closeables)) {
+            return;
+        }
         for (Closeable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                //e.printStackTrace();
+                //ignore
             }
         }
     }
@@ -37,16 +38,17 @@ public final class IOUtils {
      * Closes the IO steam quietly.
      */
     public static void closeQuietly(@Nullable final Closeable... closeables) {
-        if (EmptyUtils.isEmpty(closeables)) return;
-
+        if (EmptyUtils.isEmpty(closeables)) {
+            return;
+        }
         for (Closeable closeable : closeables) {
             try {
                 if (closeable != null) {
                     closeable.close();
                 }
             } catch (IOException e) {
-                // e.printStackTrace();
-                // swallow
+                //e.printStackTrace();
+                //swallow
             }
         }
     }
