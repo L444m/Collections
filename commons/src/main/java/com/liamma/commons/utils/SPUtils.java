3909b34e97c4aa7d96bbfa8232b8c614665068c1
Liam <gwilymma@gmail.com>
2021-06-19 17:30:46 +0800

    update dependencies of commons module.

--- commons/src/main/java/com/liamma/commons/utils/SPUtils.java
+++ commons/src/main/java/com/liamma/commons/utils/SPUtils.java
@@ -6,12 +6,13 @@ import android.content.SharedPreferences;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.gson.Gson;
 import com.liamma.commons.Commons;
 import com.liamma.commons.log.LogUtils;
 
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 /**
@@ -23,7 +24,7 @@ import java.util.Set;
 @SuppressWarnings("ApplySharedPref")
 public final class SPUtils {
 
-    public static final String DEFAULT_SP_NAME = "default_shared_preferences";
+    public static final String DEFAULT_SP_NAME = "default_sp";
 
     // Default values for retrieving data from SharedPreferences.
     public static final String DEFAULT_VALUE_STRING = "";
@@ -47,18 +48,16 @@ public final class SPUtils {
     /**
      * Gets SPUtils instance with default SP name.
      */
-    public static SPUtils getSPUtils() {
-        return getSPUtils(null);
+    @NonNull
+    public static SPUtils getInstance() {
+        return getInstance(null);
     }
 
     /**
      * Gets SPUtils instance corresponding to the specified SP name.
-     *
-     * @param spName SharedPreferences name
-     * @return SPUtils instance
      */
     @NonNull
-    public static SPUtils getSPUtils(@Nullable String spName) {
+    public static SPUtils getInstance(@Nullable String spName) {
         if (StringUtils.isBlank(spName)) {
             spName = DEFAULT_SP_NAME;
         }
@@ -72,10 +71,8 @@ public final class SPUtils {
 
     /**
      * Removes SPUtils instance corresponding to the specified SP name.
-     *
-     * @param spName SharedPreferences name
      */
-    public static void removeSPUtils(@Nullable String spName) {
+    public static void removeInstance(@Nullable String spName) {
         if (StringUtils.isBlank(spName)) {
             spName = DEFAULT_SP_NAME;
         }
@@ -85,7 +82,7 @@ public final class SPUtils {
     /**
      * Removes all SPUtils instances.
      */
-    public static void removeAllSPUtils() {
+    public static void removeAllInstance() {
         spUtilsMap.clear();
     }
 
@@ -111,70 +108,6 @@ public final class SPUtils {
         return sp.edit();
     }
 
-    public void put(@NonNull String key, @Nullable Object value, boolean isCommit) {
-        SharedPreferences.Editor editor = getEditor();
-        if (value instanceof String) {
-            editor.putString(key, (String) value);
-        } else if (value instanceof Set<?>) {
-            // risky of ? type parameter.
-            editor.putStringSet(key, (Set<String>) value);
-        } else if (value instanceof Integer) {
-            editor.putInt(key, (Integer) value);
-        } else if (value instanceof Long) {
-            editor.putLong(key, (Long) value);
-        } else if (value instanceof Float) {
-            editor.putFloat(key, (Float) value);
-        } else if (value instanceof Boolean) {
-            editor.putBoolean(key, (Boolean) value);
-        } else {
-            LogUtils.e("value = " + value + ", cannot put the type of value in SharePreference.");
-            return;
-        }
-        if (isCommit) {
-            editor.commit();
-        } else {
-            editor.apply();
-        }
-    }
-
-    @SuppressWarnings("unchecked")
-    public <T> T get(@NonNull String key, @NonNull T defaultValue) {
-        // Plan A：
-        Map<String, ?> map = sp.getAll();
-        try {
-            Object o = map.get(key);
-            return (T) o;
-        } catch (Exception e) {
-            LogUtils.e("class cast exception.");
-            return defaultValue;
-        }
-
-        // Plan B:
-        /*
-        try {
-            if (defaultValue instanceof String) {
-                sp.getString(key, (String) defaultValue);
-            } else if (defaultValue instanceof Set<?>) {
-                sp.getStringSet(key, (Set<String>) defaultValue);
-            } else if (defaultValue instanceof Integer) {
-                sp.getInt(key, (Integer) defaultValue);
-            } else if (defaultValue instanceof Long) {
-                sp.getLong(key, (Long) defaultValue);
-            } else if (defaultValue instanceof Float) {
-                sp.getFloat(key, (Float) defaultValue);
-            } else if (defaultValue instanceof Boolean) {
-                sp.getBoolean(key, (Boolean) defaultValue);
-            } else {
-                LogUtils.e("value = " + defaultValue);
-                return defaultValue;
-            }
-        } catch (Exception e) {
-            LogUtils.e("cast exception");
-        }
-        return defaultValue;
-         */
-    }
-
     /**
      * Puts string value in SharedPreferences.
      */
@@ -184,9 +117,6 @@ public final class SPUtils {
 
     /**
      * Puts string value in SharedPreferences.
-     *
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putString(@NonNull String key, @Nullable String value, boolean isCommit) {
         if (isCommit) {
@@ -221,9 +151,6 @@ public final class SPUtils {
 
     /**
      * Puts string set value in SharedPreferences.
-     *
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putStringSet(@NonNull String key, @Nullable Set<String> values, boolean isCommit) {
         if (isCommit) {
@@ -242,10 +169,7 @@ public final class SPUtils {
     }
 
     /**
-     * Gets string set from SharedPreferences.
-     *
-     * @param key          String
-     * @param defaultValue Default string set
+     * Gets string set value from SharedPreferences.
      */
     @Nullable
     public Set<String> getStringSet(@NonNull String key, @Nullable Set<String> defaultValue) {
@@ -254,9 +178,6 @@ public final class SPUtils {
 
     /**
      * Puts int value in SharedPreferences.
-     *
-     * @param key   String
-     * @param value Integer
      */
     public void putInt(@NonNull String key, int value) {
         putInt(key, value, false);
@@ -264,11 +185,6 @@ public final class SPUtils {
 
     /**
      * Puts int value in SharedPreferences.
-     *
-     * @param key      String
-     * @param value    Integer
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putInt(@NonNull String key, int value, boolean isCommit) {
         if (isCommit) {
@@ -280,8 +196,6 @@ public final class SPUtils {
 
     /**
      * Gets int value from SharedPreferences.
-     *
-     * @param key String
      */
     public int getInt(@NonNull String key) {
         return getInt(key, DEFAULT_VALUE_INT);
@@ -289,9 +203,6 @@ public final class SPUtils {
 
     /**
      * Gets int value from SharedPreferences.
-     *
-     * @param key          String
-     * @param defaultValue Default int value
      */
     public int getInt(@NonNull String key, int defaultValue) {
         return sp.getInt(key, defaultValue);
@@ -299,9 +210,6 @@ public final class SPUtils {
 
     /**
      * Puts long value in SharedPreferences.
-     *
-     * @param key   String
-     * @param value Long
      */
     public void putLong(@NonNull String key, long value) {
         putLong(key, value, false);
@@ -309,11 +217,6 @@ public final class SPUtils {
 
     /**
      * Puts long value in SharedPreferences.
-     *
-     * @param key      String
-     * @param value    Long
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putLong(@NonNull String key, long value, boolean isCommit) {
         if (isCommit) {
@@ -325,8 +228,6 @@ public final class SPUtils {
 
     /**
      * Gets long value from SharedPreferences.
-     *
-     * @param key String
      */
     public long getLong(@NonNull String key) {
         return getLong(key, DEFAULT_VALUE_LONG);
@@ -334,9 +235,6 @@ public final class SPUtils {
 
     /**
      * Gets long value from SharedPreferences.
-     *
-     * @param key          String
-     * @param defaultValue Default Long value
      */
     public long getLong(@NonNull String key, long defaultValue) {
         return sp.getLong(key, defaultValue);
@@ -344,9 +242,6 @@ public final class SPUtils {
 
     /**
      * Puts float value in SharedPreferences.
-     *
-     * @param key   String
-     * @param value Float
      */
     public void putFloat(@NonNull String key, float value) {
         putFloat(key, value, false);
@@ -354,11 +249,6 @@ public final class SPUtils {
 
     /**
      * Puts float value in SharedPreferences.
-     *
-     * @param key      String
-     * @param value    Float
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putFloat(@NonNull String key, float value, boolean isCommit) {
         if (isCommit) {
@@ -370,8 +260,6 @@ public final class SPUtils {
 
     /**
      * Gets float value from SharedPreferences.
-     *
-     * @param key String
      */
     public float getFloat(@NonNull String key) {
         return getFloat(key, DEFAULT_VALUE_FLOAT);
@@ -379,9 +267,6 @@ public final class SPUtils {
 
     /**
      * Gets float value from SharedPreferences.
-     *
-     * @param key          String
-     * @param defaultValue Default Float value
      */
     public float getFloat(@NonNull String key, float defaultValue) {
         return sp.getFloat(key, defaultValue);
@@ -389,9 +274,6 @@ public final class SPUtils {
 
     /**
      * Puts boolean value in SharedPreferences.
-     *
-     * @param key   String
-     * @param value Boolean
      */
     public void putBoolean(@NonNull String key, boolean value) {
         putBoolean(key, value, false);
@@ -399,11 +281,6 @@ public final class SPUtils {
 
     /**
      * Puts boolean value in SharedPreferences.
-     *
-     * @param key      String
-     * @param value    Boolean
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void putBoolean(@NonNull String key, boolean value, boolean isCommit) {
         if (isCommit) {
@@ -415,8 +292,6 @@ public final class SPUtils {
 
     /**
      * Gets boolean value from SharedPreferences.
-     *
-     * @param key String
      */
     public boolean getBoolean(@NonNull String key) {
         return getBoolean(key, DEFAULT_VALUE_BOOLEAN);
@@ -424,18 +299,98 @@ public final class SPUtils {
 
     /**
      * Gets boolean value from SharedPreferences.
-     *
-     * @param key          String
-     * @param defaultValue Default boolean value
      */
     public boolean getBoolean(@NonNull String key, boolean defaultValue) {
         return sp.getBoolean(key, defaultValue);
     }
 
     /**
+     * Puts data T in SharedPreferences.
+     */
+    public <T> void putData(@NonNull String key, @Nullable T t) {
+        putData(key, t, false);
+    }
+
+    /**
+     * Puts data T in SharedPreferences.
+     */
+    public <T> void putData(@NonNull String key, @Nullable T t, boolean isCommit) {
+        String json = new Gson().toJson(t);
+        if (isCommit) {
+            getEditor().putString(key, json).commit();
+        } else {
+            getEditor().putString(key, json).apply();
+        }
+    }
+
+    public <T> T getData(@NonNull String key, @Nullable T defaultValue) {
+        String json = getString(key, "");
+        if (EmptyUtils.isNotEmpty(json)) {
+            try {
+                T t = new Gson().fromJson(json, (Type) defaultValue);
+                return t;
+            } catch (Exception e) {
+                return defaultValue;
+            }
+        } else {
+            return defaultValue;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void put(@NonNull String key, @Nullable Object value, boolean isCommit) {
+        SharedPreferences.Editor editor = getEditor();
+        if (value instanceof String) {
+            editor.putString(key, (String) value);
+        } else if (value instanceof Set<?>) {
+            // risky of ? type parameter.
+            editor.putStringSet(key, (Set<String>) value);
+        } else if (value instanceof Integer) {
+            editor.putInt(key, (Integer) value);
+        } else if (value instanceof Long) {
+            editor.putLong(key, (Long) value);
+        } else if (value instanceof Float) {
+            editor.putFloat(key, (Float) value);
+        } else if (value instanceof Boolean) {
+            editor.putBoolean(key, (Boolean) value);
+        } else {
+            LogUtils.e("value = " + value + ", cannot put the type of value in SharePreference.");
+            return;
+        }
+        if (isCommit) {
+            editor.commit();
+        } else {
+            editor.apply();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(@NonNull String key, @NonNull T defaultValue) {
+        try {
+            if (defaultValue instanceof String) {
+                sp.getString(key, (String) defaultValue);
+            } else if (defaultValue instanceof Set<?>) {
+                sp.getStringSet(key, (Set<String>) defaultValue);
+            } else if (defaultValue instanceof Integer) {
+                sp.getInt(key, (Integer) defaultValue);
+            } else if (defaultValue instanceof Long) {
+                sp.getLong(key, (Long) defaultValue);
+            } else if (defaultValue instanceof Float) {
+                sp.getFloat(key, (Float) defaultValue);
+            } else if (defaultValue instanceof Boolean) {
+                sp.getBoolean(key, (Boolean) defaultValue);
+            } else {
+                LogUtils.e("value = " + defaultValue);
+                return defaultValue;
+            }
+        } catch (Exception e) {
+            LogUtils.e("cast exception");
+        }
+        return defaultValue;
+    }
+
+    /**
      * Removes a key-value pair from SharedPreferences according to the specified string key.
-     *
-     * @param key String
      */
     public void remove(@NonNull String key) {
         remove(key, false);
@@ -443,10 +398,6 @@ public final class SPUtils {
 
     /**
      * Removes a key-value pair from SharedPreferences according to the specified string key.
-     *
-     * @param key      String
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void remove(@NonNull String key, boolean isCommit) {
         if (isCommit) {
@@ -465,9 +416,6 @@ public final class SPUtils {
 
     /**
      * Clears all value from SharedPreferences.
-     *
-     * @param isCommit {@code true} means {@code commit()};
-     *                 {@code false} means {@code apply()}.
      */
     public void clear(boolean isCommit) {
         if (isCommit) {
